8929a81748759f870ff51afde0d8a66d503e3544
jmeisele <jmeisele@yahoo.com>
2023-06-06 17:22:25 +0000

    :zap: xray tracing enabled

--- src/handler.py
+++ src/handler.py
@@ -1,6 +1,13 @@
+import logging
 from typing import Dict
+
+from aws_xray_sdk.core import xray_recorder, patch_all
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+patch_all()
 
 
 def lambda_handler(event: Dict, context: Dict) -> str:
-    print(event)
+    logger.info(event)
     return "Hello from Lambda!"
