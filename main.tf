066f35ab1158310b85319b1f8446495d0cf493a6
jmeisele <jmeisele@yahoo.com>
2023-06-06 15:12:02 +0000

    :zap: lambda description

--- main.tf
+++ main.tf
@@ -78,11 +78,13 @@ data "archive_file" "python_lambda_package" {
 resource "aws_lambda_function" "lambda" {
   function_name    = "lambda"
   filename         = "lambda.zip"
+  description      = "Dumb lambda function utilizing x-ray tracing layer"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   role             = aws_iam_role.iam_for_lambda.arn
   runtime          = "python3.8"
   handler          = "handler.lambda_handler"
   timeout          = 10
+  # layers = []
 }
 
 #   ###############
