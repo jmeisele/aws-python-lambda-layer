1a5a7d1202b25c41cbcd8a1c69b54b4e6834777e
jmeisele <jmeisele@yahoo.com>
2023-06-20 10:33:16 +0000

    :construction_worker:

---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.0"
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "davinci"
+    workspaces {
+      name = "aws-python-lambda-layer"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
