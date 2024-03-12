34aade9c61433691a9466d75d961e62d4c016c78
jmeisele <jmeisele@yahoo.com>
2024-03-12 11:42:55 -0500

    :art: structure

--- providers.tf
+++ providers.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_version = "~> 1.0"
-  cloud {
-    hostname     = "app.terraform.io"
-    organization = "davinci"
-    workspaces {
-      name = "aws-python-lambda-layer"
-    }
-  }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.34"
-    }
-  }
-}
-
 provider "aws" {
   allowed_account_ids     = [var.account_id]
   access_key              = var.access_key
